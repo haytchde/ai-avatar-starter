2aee349fa6fd3bfe6321346e1d47a37a6725f732
Harrison Dempsey <hi@hdempsey.io>
2023-01-07 16:14:10 +1100

    recieving input

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,43 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const generateAction = async () => {
+    console.log('Generating...');
+  
+    // Add the fetch request
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    });
+  
+    const data = await response.json();
+
+    // If model still loading, drop that retry time
+    if (response.status === 503) {
+      console.log('Model is loading still :(.')
+      return;
+    }
+
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+
+    setImg(data.image);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -18,9 +53,16 @@ const Home = () => {
               Turn me into anyone you want! Use "haytchde" in the prompt
             </h2>
           </div>
-          {/* Add prompt container here */}
           <div className="prompt-container">
-            <input className="prompt-box" />
+            <input className="prompt-box" value={input} onChange={onChange} />
+            {/* Add your prompt button in the prompt container */}
+            <div className="prompt-buttons">
+              <a className="generate-button">
+                <div className="generate" onClick={generateAction}>
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
